49adede84fc29682a0e14f8efb4cc2cf5e919df6
Justin Langlinais <justinclanglinais@gmail.com>
2020-10-04 11:25:58 -0700

    feat(App): add button and loggedIn value rendered on screen

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ class App extends React.Component {
   render () {
     return (
       <div className="App">
+        {this.state.loggedIn ? <h2>You are logged in!</h2> : <h2>You are logged out :(</h2>}
+        <button onClick={() => this.setState({loggedIn : !this.state.loggedIn})}>
+          Toggle Login/Logout
+        </button>
         <Content loggedIn={this.state.loggedIn} />
       </div>
     );
