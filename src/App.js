81c4b3ea437dc8f3a9043657cabaccbec523731c
Justin Langlinais <justinclanglinais@gmail.com>
2020-10-07 12:00:16 -0700

    feat(App): convert state to hooks

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Content from './components/Content.js'
 
-class App extends React.Component {
-  state = {
-    loggedIn : true,
-    loaded : false
-  }
-  render () {
-    return (
-      <div className="App">
-        <div>
-          {this.state.loggedIn ? <h2>You are logged in!</h2> : <h2>You are logged out :(</h2>}
-          <button onClick={() => this.setState({loggedIn : !this.state.loggedIn})}>
-            Toggle Login/Logout
-          </button>
-        </div>
-        <div>
-        {this.state.loaded ? <h2>Content is loaded</h2> : <h2>Content not loaded</h2>}
-          <button onClick={() => this.setState({loaded : !this.state.loaded})}>
-            Toggle Content Loaded
-          </button>
-        </div>
-        <Content loggedIn={this.state.loggedIn} loaded={this.state.loaded} />
+function App () {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  
+  return (
+    <div className="App">
+      <div>
+        {loggedIn ? <h2>You are logged in!</h2> : <h2>You are logged out :(</h2>}
+        <button onClick={() => setLoggedIn(!loggedIn)}>
+          Toggle Login/Logout
+        </button>
       </div>
-    );
-  }
+      <div>
+        {loaded ? <h2>Content is loaded</h2> : <h2>Content not loaded</h2>}
+        <button onClick={() => setLoaded(!loaded)}>
+          Toggle Content Loaded
+        </button>
+      </div>
+      <Content loggedIn={loggedIn} loaded={loaded} />
+    </div>
+  );
 }
 
 export default App;
